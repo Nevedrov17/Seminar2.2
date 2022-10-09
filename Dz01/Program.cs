d0ced51bbc2744c3c4510fa78e239203636f72d5
NEVA <neva.vka@yandex.ru>
2022-10-09 23:42:00 +0300

    задача 19

---
+++ Dz01/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 0;
+int c = 0;
+if (a > 9999)
+{
+    while (a > 99)
+    {
+        a= a/10;
+    }
+}
+else Console.WriteLine("Число не 5-ти значное");
