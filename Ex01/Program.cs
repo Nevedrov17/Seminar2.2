4e3e11e74ac0b02bf5460ea02cf5e2fe630e4bae
NEVA <neva.vka@yandex.ru>
2022-10-09 15:07:39 +0300

    Занятие на семинаре

--- Ex01/Program.cs
+++ Ex01/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿Console.Clear();
+
+//1) МАССИВ ИЗ СТРОК
+
+string name1 = "Bob";
+string name2 = "Shmob";
+string name3 = "Shirli";
+string name4 = "Mirli";
+
+string [] names = { name1, name2, name3, name4};
+Console.WriteLine(names[1]);
+names[1]= "Sheldin";
+Console.WriteLine(names[1]);
+
+//2) МАССИВ ИЗ ЦЕЛЫХ ЧИСЕЛ
+
+//int[] age = new int[5];
+/*int[] age = {1,2,3,4,5};
+Console.WriteLine(age[1]);
+age[1]=age[1]+5;
+Console.WriteLine(age[1]);
+Console.WriteLine(age.Length);*/
+
+for(int i = 0; i < names.Length; i++ )
+Console.WriteLine(names[i]);
+
+
+
