ff47c19dcc6e755ec0f1645a1c3f8a26f0d2e4db
NEVA <neva.vka@yandex.ru>
2022-10-10 11:26:45 +0300

    Задача 19 палиндром

--- Dz01/Program.cs
+++ Dz01/Program.cs
@@ -2,13 +2,20 @@
 
 Console.Write("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-int b = 0;
-int c = 0;
-if (a > 9999)
-{
+
+int b = a%100;
+int x1 = b%10;
+int x2 = b/10;
+
     while (a > 99)
     {
         a= a/10;
     }
+int y1 = a/10;
+int y2 = a%10;
+
+if (x1 == y1 && x2 == y2)
+{
+    Console.WriteLine("Число является полиндромом");
 }
-else Console.WriteLine("Число не 5-ти значное");
+else Console.WriteLine("Число не является полиндромом");
